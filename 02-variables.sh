e1b98f0782f1e226d9608bfa80cafe59414cb942
Kalyan Kumar Voleti <kalyankumarvoleti@gmail.com>
2025-07-17 19:07:53 +0530

    variables

--- 02-variables.sh
+++ 02-variables.sh
@@ -3,9 +3,9 @@
 #conversation between two friends
 
 echo "Calvin: Hey Levi, excited about heading to the new city tomorrow"
-echo "Levi: Totally Fresh start, new vibes. Got everything packed"
+echo "Lee: Totally Fresh start, new vibes. Got everything packed"
 echo "Calvin: Almost. Whats the first thing you wanna check out there"
-echo "Levi: Probably the local cafes. You"
+echo "Lee: Probably the local cafes. You"
 echo "Calvin: The parks, for sure. Gotta explore those trails."
-echo "Levi: Sounds good. Lets plan some adventures once we settle in."
+echo "Lee: Sounds good. Lets plan some adventures once we settle in."
 echo "Calvin: Absolutely. New city, new stories"
